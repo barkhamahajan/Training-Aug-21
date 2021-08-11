51ead6b93e72b09f788788fdacdd278ce336e8f9
barkhamahajan <88368206+barkhamahajan@users.noreply.github.com>
2021-08-11 17:29:30 +0530

    changes update 3

--- SQL/Barkha Mahajan/Day2/Update tasks/Update3.sql
+++ SQL/Barkha Mahajan/Day2/Update tasks/Update3.sql
@@ -1,5 +1,5 @@
 UPDATE Employees 
-SET Email='not availableQue3' , CommissionPct =0.100 
+SET Email='Not Available (Task3)' , CommissionPct =0.100 
 WHERE DepartmentID=110
 
 SELECT * FROM Employees where DepartmentID=110

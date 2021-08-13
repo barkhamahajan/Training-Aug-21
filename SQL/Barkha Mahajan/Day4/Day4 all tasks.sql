42978ece2519e281390ded6d615c9de16c7bbdc5
barkhamahajan <88368206+barkhamahajan@users.noreply.github.com>
2021-08-13 14:38:09 +0530

    Create Day4 all tasks.sql

---
+++ SQL/Barkha Mahajan/Day4/Day4 all tasks.sql
@@ -0,0 +1,51 @@
+SELECT * FROM dbo.employees
+
+--Q1
+
+SELECT FirstName+' '+LastName 'Name', Salary, RANK() OVER (ORDER BY Salary) 'Salary Ranking' FROM dbo.Employees
+
+--Q2
+
+SELECT * FROM (
+SELECT FirstName+' '+LastName 'Name', Salary, Sal_Rank = RANK() OVER (ORDER BY Salary DESC) FROM dbo.Employees
+) temp
+WHERE Sal_Rank=4
+
+--Q3
+
+SELECT DepartmentID, Total_Salary=SUM(Salary) from dbo.Employees
+GROUP BY DepartmentID
+
+--Q4
+
+SELECT DepartmentID, Total_Salary=SUM(Salary) from dbo.Employees
+GROUP BY DepartmentID
+ORDER BY Total_Salary DESC
+
+--Q5
+
+SELECT DISTINCT DepartmentID,  dept_max_sal=(MAX(Salary) OVER( PARTITION BY DepartmentID)) FROM dbo.Employees
+ORDER BY dept_max_sal ASC
+
+--Q6
+
+SELECT DISTINCT DepartmentID, dept_min_sal=(MIN(Salary) OVER( PARTITION BY DepartmentID)) FROM dbo.Employees
+ORDER BY dept_min_sal ASC
+
+--Q7
+SELECT * FROM 
+(
+SELECT DepartmentID, total_dept_sal=(SUM(Salary)) FROM dbo.Employees
+GROUP BY DepartmentID
+) temp
+WHERE total_dept_sal>50000
+ORDER BY total_dept_sal DESC 
+
+--OR
+
+SELECT * FROM
+(
+SELECT DISTINCT DepartmentID, total_dept_sal=(SUM(Salary) OVER ( PARTITION BY DepartmentID)) FROM dbo.Employees
+) temp
+WHERE total_dept_sal>50000
+ORDER BY total_dept_sal DESC

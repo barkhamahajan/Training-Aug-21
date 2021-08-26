e91d69a08c45e9f57208172ed2a4bf4c290fa5f0
barkhamahajan <88368206+barkhamahajan@users.noreply.github.com>
2021-08-26 18:40:55 +0530

    Create day6_subquery_view_2.sql

---
+++ SQL/Barkha Mahajan/day6/day6_subquery_view_2.sql
@@ -0,0 +1,59 @@
+SELECT * FROM Employees
+SELECT * FROM Departments
+SELECT * FROM JobHistory
+
+--Q5
+
+CREATE VIEW EMPOYEE_LONDON 
+AS
+SELECT FirstName+ ' '+LastName 'NAME', JobId, E.DepartmentID, D.DepartmentName FROM Employees E JOIN Departments D
+ON E.DepartmentID=D.DepartmentID
+WHERE E.DEPARTMENTID=(
+SELECT DEPARTMENTID FROM DEPARTMENTS WHERE LOCATIONID =
+(SELECT LocationID FROM Locations WHERE CITY='LONDON')
+)
+
+SELECT * FROM EMPOYEE_LONDON 
+
+--Q6
+
+CREATE VIEW DEPARTMENT_COUNT
+AS
+SELECT D.DepartmentID,D.DEPARTMENTNAME ,COUNT (*) 'TOTAL EMP COUNT' FROM Employees E, DEPARTMENTS D
+WHERE E.DepartmentID=D.DepartmentID
+GROUP BY D.DepartmentID,D.DEPARTMENTNAME
+
+SELECT * FROM DEPARTMENT_COUNT
+
+--Q7
+
+CREATE VIEW DEPT_90_EXP
+AS
+SELECT E.EMPLOYEEID, E.JobId, DATEDIFF(DAY,J.StartDate, J.EndDate)'DIFF IN DAYS'  FROM Employees E JOIN Departments D
+ON E.DepartmentID=D.DepartmentID JOIN JobHistory J
+ON E.EmployeeID=J.EmployeeID
+WHERE J.DepartmentID=90
+
+SELECT * FROM DEPT_90_EXP
+
+--Q8
+
+CREATE VIEW MANAGER_DEPT_CITY
+AS
+SELECT DISTINCT D.DepartmentName, M.FirstName, L.CITY FROM Employees E JOIN Departments D
+ON E.DepartmentID=D.DepartmentID JOIN Employees M
+ON E.ManagerID=M.EmployeeID JOIN Locations L
+ON D.LocationID=L.LocationID
+
+SELECT * FROM MANAGER_DEPT_CITY
+
+--Q9
+
+CREATE VIEW MANAGER_EXP_MORE_15_YEARS
+AS
+SELECT DISTINCT  D.DepartmentName, M.FirstName+' '+M.LastName 'NAME', M.HireDate,M.Salary FROM Employees E JOIN Employees M
+ON E.ManagerID=M.EmployeeID JOIN Departments D
+ON E.DepartmentID=D.DepartmentID
+WHERE DATEDIFF(YEAR, M.HIREDATE, GETDATE())>15
+
+SELECT * FROM MANAGER_EXP_MORE_15_YEARS

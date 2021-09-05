a6828f761d4a8d93afcf6ab144cbccef2f2cf130
barkhamahajan <88368206+barkhamahajan@users.noreply.github.com>
2021-09-05 21:50:57 +0530

    Update and rename Day5 all tasks.sql to Day5_all_tasks.sql

--- SQL/Barkha Mahajan/DAY5/Day5_all_tasks.sql
+++ SQL/Barkha Mahajan/DAY5/Day5_all_tasks.sql
@@ -22,29 +22,30 @@ VALUES ('John', 'Abraham', 1000000, '2013-01-01 00:00:00', 'Banking', NULL),
  JOININGG_DATE=(CONVERT(Varchar(13) , JOINING_DATE, 106)+' '+ RIGHT(CONVERT(VARCHAR(20), JOINING_DATE, 100), 7)), DEPARTMENT, MANAGER_ID 
  from Employee
 
---Q1
+--Q1 Get difference between JOINING_DATE and INCENTIVE_DATE from employee and incentives table
 
  SELECT e.Employee_ID, DIFF_IN_MONTHS=CONVERT(VARCHAR(5),DATEDIFF(MM, e.joining_date, i.incentive_date) )+' Months and '+ CONVERY(VARCHAR(5),DATEDIFF(YEARS, e.joining_date, i.incentive_date))+' Years'
  from Employee e join Incentive i
 on e.EMPLOYEE_ID=i.EMPLOYEE_REF_ID
 
---Q2
+--Q2 Select first_name, incentive amount from employee and incentives table for those employees who have incentives and incentive amount greater than 3000
 
 Select e.FIRST_NAME, i.INCENTIVE_AMOUNT FROM Employee e JOIN Incentive i
 ON e.EMPLOYEE_ID=i.EMPLOYEE_REF_ID
 WHERE INCENTIVE_AMOUNT>3000
-
---Q3
+ 
+--Q3 Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get incentives.
 
 Select e.FIRST_NAME, i.INCENTIVE_AMOUNT FROM Employee e LEFT OUTER JOIN Incentive i
 ON e.EMPLOYEE_ID=i.EMPLOYEE_REF_ID
 
---Q4
+--Q4 Select EmployeeName, ManagerName from the employee table.
 
 SELECT Employee=e.FIRST_NAME , Manager= m.fIRST_NAME FROM Employee e LEFT OUTER JOIN Employee m
 ON e.MANAGER_ID=m.EMPLOYEE_ID
 
---Q5
+--Q5 Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get 
+--incentives and set incentive amount as 0 for those employees who didn’t get incentives.
 
 SELECT e.FIRST_NAME, ISNULL(i.INCENTIVE_AMOUNT,0) FROM Employee e LEFT OUTER JOIN Incentive i
 ON e.EMPLOYEE_ID=i.EMPLOYEE_REF_ID

87d753a71ced9ad1603c47a521f7f8627e7ab5eb
barkhamahajan <88368206+barkhamahajan@users.noreply.github.com>
2021-09-05 22:39:46 +0530

    Update day7_cte_task2.sql

--- SQL/Barkha Mahajan/day7/day7_cte_task2.sql
+++ SQL/Barkha Mahajan/day7/day7_cte_task2.sql
@@ -1,8 +1,8 @@
 SELECT * FROM Employee
 SELECT * FROM Incentive
 
---DAY5
---Q1
+--DAY5 using cte
+--Q1 Get difference between JOINING_DATE and INCENTIVE_DATE from employee and incentives table
 
 WITH CTE_INCENTIVE_MONTH 
 AS 
@@ -12,7 +12,7 @@ ON E.EMPLOYEE_ID=I.EMPLOYEE_REF_ID
 )
 SELECT * FROM CTE_INCENTIVE_MONTH
 
---Q2
+--Q2 Select first_name, incentive amount from employee and incentives table for those employees who have incentives and incentive amount greater than 3000
 
 WITH CTE_INCENTIVE_OVER_3000
 AS
@@ -23,7 +23,7 @@ WHERE I.INCENTIVE_AMOUNT>3000
 )
 SELECT * FROM CTE_INCENTIVE_OVER_3000
 
---Q3
+--Q3 Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get incentives.
 
 WITH CTE_ALL_EMPS
 AS
@@ -33,7 +33,7 @@ ON E.EMPLOYEE_ID=I.EMPLOYEE_REF_ID
 )
 SELECT * FROM CTE_ALL_EMPS
 
---Q4
+--Q4 Select EmployeeName, ManagerName from the employee table.
 
 WITH CTE_EMP_MNGR
 AS
@@ -43,7 +43,9 @@ ON E.MANAGER_ID=M.EMPLOYEE_ID
 )
 SELECT * FROM CTE_EMP_MNGR
 
---Q5
+--Q5Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get incentives 
+--and set incentive amount as 0 for those employees who didn’t get incentives.
+
 
 WITH CTE_EMPS
 AS
